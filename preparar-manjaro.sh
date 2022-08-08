1fffc0f4df4fcb19500fece69aa91cb89e8fe5db
José Ricardo Silveira <ricardusilveira@gmail.com>
2022-08-08 00:29:55 -0300

    adiciona ksnip e miniconda script

--- preparar-manjaro.sh
+++ preparar-manjaro.sh
@@ -10,3 +10,13 @@ sudo ln -s /var/lib/snapd/snap /snap
 sudo snap install pycharm-professional --classic
 sudo snap install code --classic
 sudo snap install kate --classic
+sudo snap install ksnip
+
+# install Miniconda
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda init bash
+~/miniconda3/bin/conda init zsh
+
